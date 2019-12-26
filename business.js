4899b4a52fb379dbf9f740e98dbc0eb91aa530f6
ajhxk <753663797@qq.com>
2019-12-26 23:21:07 +0800

    add update demo

--- business.js
+++ business.js
@@ -6,15 +6,26 @@ const print = require('./lib/print')
 class Main {
     constructor (opts = {}) {
         this.options = opts;
-        this._ware = this._buildware();
+        this._ware_create = this._build_create();
+        this._ware_update = this._build_update();
     }
 
-    _buildware () {
+    _build_create () {
         const _ware = new Middleware();
         _ware.use(this._base_step_1)
             .use(this._base_step_2)
-            .use(this._base_step_3);
-        return _ware 
+            .use(this._base_step_3)
+            .use(this._create_step)
+        return _ware; 
+    }
+
+    _build_update () {
+        const _ware = new Middleware();
+        _ware.use(this._base_step_1)
+            .use(this._base_step_2)
+            .use(this._base_step_3)
+            .use(this._update_step)
+        return _ware; 
     }
 
     _base_step_1 (options, next) {
@@ -38,10 +49,28 @@ class Main {
         print(`end ${stepName}`)
     }
 
+    _create_step (options, next) {
+        print('do create', options);
+        next();
+        print('create end', options);
+    }
+
+    _update_step (options, next) {
+        print('do update', options);
+        next();
+        print('update end', options);
+    }
+
     create () {
-        return this._ware.handleRequest(this.options);
+        return this._ware_create.handleRequest(this.options);
+    }
+
+    update () {
+        return this._ware_update.handleRequest(this.options);
     }
 
 }
 
-new Main({a: 'aa'}).create();
+const main = new Main({aa: 'aa'});
+main.create();
+main.update();
